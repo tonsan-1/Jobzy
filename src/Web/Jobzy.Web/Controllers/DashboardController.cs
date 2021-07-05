3dadf8044dafd37091b911d1e222fdcd7415925d
Stoyan Kostadinov <tonsan221@gmail.com>
2021-07-05 11:02:06 +0300

    Implement job post view

---
+++ src/Web/Jobzy.Web/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿namespace Jobzy.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : BaseController
+    {
+        [Authorize(Roles = "Administrator, Employer")]
+        public IActionResult PostJob()
+        {
+            return this.View();
+        }
+    }
+}
