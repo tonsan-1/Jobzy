f0e01ef1a90afe12231e4e5101795ba6eb66ffc0
Stoyan Kostadinov <tonsan221@gmail.com>
2021-07-05 14:40:56 +0300

    Add job input model for job post

---
+++ src/Web/Jobzy.Web.ViewModels/Job/JobInputModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Jobzy.Web.ViewModels.Job
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Jobzy.Common;
+
+    public class JobInputModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Title { get; set; }
+
+        [Range(0, 1)]
+        public JobType JobType { get; set; }
+
+        [Range(0, 5)]
+        public JobCategory JobCategory { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Budget { get; set; }
+
+        [Required]
+        [StringLength(2048, MinimumLength = 5)]
+        public string Description { get; set; }
+
+        public string Tags { get; set; }
+    }
+}
