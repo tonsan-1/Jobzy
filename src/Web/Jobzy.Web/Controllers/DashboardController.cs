8f77969fc2ac1cc8ae59732cc54970d1c142b84e
unknown <68417157+tonsan221@users.noreply.github.com>
2021-07-07 19:17:22 +0300

    Separating Job Controller from Dashboard Controller

--- src/Web/Jobzy.Web/Controllers/DashboardController.cs
+++ src/Web/Jobzy.Web/Controllers/DashboardController.cs
@@ -1,11 +1,9 @@
 ﻿namespace Jobzy.Web.Controllers
 {
-    using System;
     using System.Threading.Tasks;
 
     using Jobzy.Data.Models;
     using Jobzy.Services.Interfaces;
-    using Jobzy.Web.ViewModels.Job;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
@@ -21,51 +19,6 @@
         {
             this.freelancePlatformManager = freelancePlatformManager;
             this.userManager = userManager;
-        }
-
-        [Authorize(Roles = "Administrator, Employer")]
-        public IActionResult PostJob() => this.View();
-
-        [HttpPost]
-        [Authorize(Roles = "Administrator, Employer")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> PostJob(JobInputModel input)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return null;
-            }
-
-            if (!(await this.userManager.GetUserAsync(this.User) is Employer currentUser))
-            {
-                return this.Forbid();
-            }
-
-            var freelancePlatformBalance = await this.freelancePlatformManager.BalanceManager.GetFreelancePlatformBalanceAsync();
-            var currentUserBalance = this.freelancePlatformManager.BalanceManager.FindById(currentUser.Id);
-
-            try
-            {
-                await this.freelancePlatformManager.BalanceManager.TransferMoneyAsync(
-                    currentUserBalance, freelancePlatformBalance, input.Budget);
-            }
-            catch (Exception e)
-            {
-                throw new ArgumentException("Error");
-            }
-
-            await this.freelancePlatformManager.JobManager.AddAsync(input, currentUser);
-            return this.Redirect("/");
-        }
-
-        [Authorize(Roles = "Administrator, Employer")]
-        public async Task<IActionResult> ManageJob()
-        {
-            var user = await this.userManager.GetUserAsync(this.User);
-
-            var jobs = this.freelancePlatformManager.JobManager.GetAllUserJobPosts(user.Id);
-
-            return this.View(jobs);
         }
 
         [Authorize(Roles = "Administrator, Employer")]
---
+++ src/Web/Jobzy.Web/Controllers/JobController.cs
@@ -0,0 +1,73 @@
+﻿namespace Jobzy.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Jobzy.Data.Models;
+    using Jobzy.Services.Interfaces;
+    using Jobzy.Web.ViewModels.Job;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class JobController : Controller
+    {
+        private readonly IFreelancePlatformManager freelancePlatformManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public JobController(
+            IFreelancePlatformManager freelancePlatformManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.freelancePlatformManager = freelancePlatformManager;
+            this.userManager = userManager;
+        }
+
+        [Route("/Dashboard/Jobs/MyJobs")]
+        [Authorize(Roles = "Administrator, Employer")]
+        public async Task<IActionResult> MyJobs()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            var jobs = this.freelancePlatformManager.JobManager.GetAllUserJobPosts(user.Id);
+
+            return this.View(jobs);
+        }
+
+        [Route("/Dashboard/Jobs/Add")]
+        [Authorize(Roles = "Administrator, Employer")]
+        public IActionResult AddJob() => this.View();
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator, Employer")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddJob(JobInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return null;
+            }
+
+            if (!(await this.userManager.GetUserAsync(this.User) is Employer currentUser))
+            {
+                return this.Forbid();
+            }
+
+            var freelancePlatformBalance = await this.freelancePlatformManager.BalanceManager.GetFreelancePlatformBalanceAsync();
+            var currentUserBalance = this.freelancePlatformManager.BalanceManager.FindById(currentUser.Id);
+
+            try
+            {
+                await this.freelancePlatformManager.BalanceManager.TransferMoneyAsync(
+                    currentUserBalance, freelancePlatformBalance, input.Budget);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Error");
+            }
+
+            await this.freelancePlatformManager.JobManager.AddAsync(input, currentUser);
+            return this.Redirect("/");
+        }
+    }
+}
