26568dc6cb70e7659374851d144fdfe39703f14f
unknown <68417157+tonsan221@users.noreply.github.com>
2021-07-16 17:56:53 +0300

    Add jobs and offers list to employer and freelancer

--- src/Data/Jobzy.Data.Models/Employer.cs
+++ src/Data/Jobzy.Data.Models/Employer.cs
@@ -6,7 +6,9 @@
     {
         public virtual List<EmployerTag> EmployerTags { get; set; } = new List<EmployerTag>();
 
-        public virtual List<Contract> EmployerContracts { get; set; } = new List<Contract>();
+        public virtual List<Contract> Contracts { get; set; } = new List<Contract>();
+
+        public virtual List<Job> Jobs { get; set; } = new List<Job>();
 
         // implement more properties such as
         // Open Jobs, Reviews..etc
--- src/Data/Jobzy.Data.Models/Freelancer.cs
+++ src/Data/Jobzy.Data.Models/Freelancer.cs
@@ -6,6 +6,8 @@
     {
         public virtual List<Contract> Contracts { get; set; } = new List<Contract>();
 
+        public virtual List<Offer> Offers { get; set; } = new List<Offer>();
+
         public virtual List<FreelancerTag> FreelancerTags { get; set; } = new List<FreelancerTag>();
 
         // implement more properties such as
