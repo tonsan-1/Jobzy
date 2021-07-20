6cbad5ba8527d75fd3e2c1652d622af4ac0b7f6a
unknown <68417157+tonsan221@users.noreply.github.com>
2021-07-20 15:33:19 +0300

    Logout fixed and now only using the identity post method on button click

---
+++ src/Web/Jobzy.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,32 @@
+﻿namespace Jobzy.Web.Areas.Identity.Pages.Account
+{
+    using System.Threading.Tasks;
+
+    using Jobzy.Data.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await this.signInManager.SignOutAsync();
+
+            return this.Redirect("/");
+        }
+    }
+}
